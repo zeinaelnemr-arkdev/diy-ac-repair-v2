e0a6092730d334eb50bd6b69355341a8baf471e5
zeinaelnemr-arkdev <zeina.elnemr@arkdev.net>
2026-07-31 19:18:19 +0300

    Fix grammar in cart save hint (mobile)

--- lib/e_review_your_order/mobile/e_review_your_order_mobile/e_review_your_order_mobile_widget.dart
+++ lib/e_review_your_order/mobile/e_review_your_order_mobile/e_review_your_order_mobile_widget.dart
@@ -933,7 +933,7 @@ class _EReviewYourOrderMobileWidgetState
                           ),
                         ),
                         Text(
-                          'Please save it before continue. you can customize tools if you needed.',
+                                                    'Please save before continuing. You can customize tools if you need to.',
                           style: FlutterFlowTheme.of(context)
                               .bodyMedium
                               .override(
